35c2a50409913fc92d33ca71df72bf69735e9248
ramsuz <benalaya.ramzi@gmail.com>
2018-07-13 16:52:18 +0200

    Create oauth-constants.js
    new

---
+++ oauth-constants.js
@@ -0,0 +1,7 @@
+export default {
+  LOGIN_SUCCESS_EVENT : 'oauth:loginSuccess',
+  INVALID_TOKEN_EVENT : 'oauth:invalidToken',
+  OAUTH_STORAGE_JWT_KEY : 'oauth:jwtToken',
+  OAUTH_TOKEN_TYPE : 'Bearer',
+  AUTHORIZATION_HEADER : 'Authorization'
+};
