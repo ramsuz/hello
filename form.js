73523a18b3b140009d876ce00596984120b8e53a
ramsuz <benalaya.ramzi@gmail.com>
2018-08-13 23:54:55 +0200

    form.js
    form

---
+++ form.js
@@ -0,0 +1,15 @@
+  <div class='hello-card'>
+    <div class="form-horizontal" role="form">
+      <div class="form-group">
+        <label for="inputType" class="col-sm-2 control-label">Label</label>
+        <div class="col-sm-2">
+          <input type="text" class="form-control" id="input" placeholder="Input text">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">Sign in</button>
+        </div>
+      </div>
+    </div>
+  </div>
